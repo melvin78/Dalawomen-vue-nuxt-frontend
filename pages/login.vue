7e1c7e64c4afb38d67953883e2bc508563b13b74
melvin78 <67906796+melvin78@users.noreply.github.com>
2021-04-02 23:04:31 +0300

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -1,111 +1,77 @@
 <template>
-
   <v-container class="centered">
-
+    <div class="child">
       <v-row align="center" justify="center">
         <v-col
           cols="12"
           lg="6"
           md="8"
-          sm="10">
-          <v-card elevation="13" shaped>
-            <v-card-title>Enter Your Email Address and Password To Login</v-card-title>
-            <v-card-subtitle>
-              If you wish to test
+          sm="10"
+        >
+          <v-card elevation="13"
+                  shaped>
+            <v-card-title>Fill in the form to register</v-card-title>
+            <v-card-subtitle>You will be assigned the role of a member by default. If you wish to test
               the admin features,login with the following credentials:<br>
-              <strong>Email</strong> Miles95@example.org<br>
-              <strong>Password:</strong> password<br>
+              <strong>Email: Miles95@example.org</strong><br>
+              <strong>Password: password</strong><br>
             </v-card-subtitle>
-            <ValidationObserver ref="loginform" v-slot="{ handleSubmit }">
+
+            <validation-observer ref="loginform" v-slot="{ handleSubmit}">
               <form @submit="handleSubmit(login)">
-                <ValidationProvider v-slot="{ errors }" name="E-mail" rules="required|email" vid="email">
+                <ValidationProvider v-slot="{ errors }" name="email" rules="required" vid="email">
                   <v-text-field
                     v-model="email"
-
                     :error-messages="errors"
                     class="mr-4 ml-4 pt-8"
                     dense
-                    label="email"
-                    name="email"
+                    label="Name"
+                    name="name"
                     outlined
-                    placeholder="Email Address"
                     required
                   ></v-text-field>
-
-
                 </ValidationProvider>
-                <ValidationProvider v-slot="{ errors }" name="Password" rules="required" vid="password">
+                <ValidationProvider v-slot="{ errors }" name="password" rules="required|min:8" vid="password">
                   <v-text-field
                     v-model="password"
                     :error-messages="errors"
-                    class="mr-4 ml-4 pt-3"
+                    class="mr-4 ml-4 pt-8"
                     dense
-                    label="password"
-                    name="password"
+                    label="Password"
                     outlined
-                    placeholder="Password"
-                    required
+                    type="password"
+
                   ></v-text-field>
-
                 </ValidationProvider>
-                <v-divider></v-divider>
-                <v-row class="pt-5" justify="center">
 
+                <v-divider></v-divider>
+                <v-row class="pt-3" justify="end">
                   <v-card-actions>
                     <v-btn
-
                       class="mr-4"
                       color="success"
                       x-large
-
                       @click="login"
                     >
 
-                      LOGIN
+                  LOGIN
                     </v-btn>
 
-
                   </v-card-actions>
-
                 </v-row>
 
 
               </form>
-            </ValidationObserver>
+
+            </validation-observer>
+
           </v-card>
-<!--          <div class="pt-5">-->
-<!--            <v-sheet-->
-<!--              elevation="1"-->
-
-<!--              rounded-->
-<!--              shaped-->
-<!--            >-->
-<!--              Dont have an account? Click on register to create one-->
-<!--              <v-btn-->
-<!--                class="mr-4"-->
-<!--                color="success"-->
-<!--                text-->
-<!--                type="submit"-->
-<!--                x-large-->
-<!--                @click="registerMe"-->
-<!--              >-->
-<!--                <v-icon-->
-<!--                  dark-->
-<!--                  right-->
-<!--                >-->
-<!--                  mdi-plus-->
-<!--                </v-icon>-->
-<!--                REGISTER-->
-<!--              </v-btn>-->
-<!--            </v-sheet>-->
-<!--          </div>-->
-
         </v-col>
-
-
       </v-row>
-
+    </div>
   </v-container>
+
+
 </template>
 
 <script>
@@ -134,10 +100,9 @@ export default {
   methods: {
 
 
-   login: function () {
+    login: function () {
 
-      this.errors = null;
-      this.message = null;
+
 
       const payload = {
         password: this.password,
@@ -145,24 +110,17 @@ export default {
 
       }
 
-      this.$auth.loginWith('laravelSanctum', {
-
-        data: payload
-      })
+      this.$auth.loginWith('laravelSanctum', {data: payload})
         .then(() => {
-          // this.$router.push('/dashboard',()=>this.$router.go(0) )
           this.$router.push('/dashboard')
         })
         .catch(() => {
-        this.$refs.loginform.setErrors(this.FormError.errors);
-      })
+          this.$refs.loginform.setErrors(this.FormError.errors);
+        })
 
 
     },
-    //
-    // check: function () {
-    //   console.log(this.FormError.errors.email);
-    // }
+
 
     registerMe() {
       this.$router.push('/register')
@@ -178,23 +136,10 @@ export default {
 
 
   },
-  mounted() {
-
-  }
-
 
 }
 
 
 </script>
 
-<style scoped>
 
-/*.centered{*/
-/* display: flex;*/
-/*  justify-content: center;*/
-/*  align-items: center;*/
-/*}*/
-
-
-</style>
